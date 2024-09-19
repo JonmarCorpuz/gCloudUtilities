52c7937bc9e4a0f4f58db6d63ac2f526eeaa9188
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-18 20:46:45 -0400

    Update CreateVPC.sh

--- CreateVPC/CreateVPC.sh
+++ CreateVPC/CreateVPC.sh
@@ -38,14 +38,16 @@ while getopts ":p:" opt; do
 done
 
 #
-if [ $OPTIND -eq 1 ]; then
+if [ $OPTIND -eq 1 ]; 
+then
     echo -e "${RED}[ERROR 3]${WHITE} Usage: ./CreateVPC.sh -p <PROJECT_ID>" && echo "" &&  exit 1
 fi
 
 ######################################### SET PROJECT ###########################################
 
 # Verify that the provided Project ID exists
-if gcloud projects describe $2 &> /dev/null; then
+if gcloud projects describe $2 &> /dev/null; 
+then
     # Set the project
     gcloud config set project $2
     echo -e "${GREEN}[SUCCESS]${WHITE} The project has been set."
@@ -56,20 +58,70 @@ fi
 #################################### VIRTUAL PRIVATE NETWORK ####################################
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name that would you like to give your Virtaul Private Network:) " PrivateNetworkName
+#read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name that would you like to give your virtual private network:) " InstanceTemplateName
 
-if gcloud compute networks describe $PrivateNetworkName &> /dev/null ; then
-    echo "" && echo -e "${RED}[ERROR 7]${WHITE} A Virtual Private Network called ${PrivateNetworkName} already exists in your project." && echo "" && exit 1
+#
+if gcloud compute networks describe "${2}-vpc" &> /dev/null ; 
+then
+    echo "" && echo -e "A Virtual Private Network called ${PrivateNetworkName} already exists in your project." && echo ""
 fi
 
 #
-#gcloud compute networks create $PrivateNetworkName \
-#    --bgp-routing-mode=regional
+read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the region where you want your virtual private network to reside in:) " NetworkRegion
+
+#
+gcloud compute networks create "${2}-vpc" \
+    --subnet-mode custom 
 
 # 
-#gcloud compute networks subnets create \
-#    --network=$PrivateNetworkName \
-#    --region=
+gcloud compute networks subnets create "${2}-vpc-subnet" \
+    --network $2-vpc \
+    --region $NetworkRegion \
+    --purpose PRIVATE \
+    --range 10.10.0.0/24 
+
+######################################### FIREWALL RULES ########################################
+
+#
+if gcloud compute firewall-rules describe "${2}-vpc-allow"; 
+then
+    echo "" && echo -e "A Virtual Private Network called vpc-allow already exists in your project." && echo ""
+else
+    gcloud compute firewall-rules create ${2}-vpc-allow \
+        --network $2-vpc \
+        --action allow \
+        --source-ranges 0.0.0.0/0 \
+        --target-tags $2-vpc-allow \
+        --rules tcp:80
+fi 
+
+#
+if gcloud compute firewall-rules describe "${2}-http-health-check"; 
+then
+    echo "" && echo -e "A Virtual Private Network called http-health-check already exists in your project." && echo ""
+else
+    gcloud compute firewall-rules create ${2}-http-health-check \
+        --action allow \
+        --direction ingress \
+        --source-ranges 0.0.0.0/0 \
+        --target-tags $2-http-health-check \
+        --network $2-vpc \
+        --rules tcp:80
+fi 
+
+#
+if gcloud compute firewall-rules describe "${2}-http-public-access"; 
+then
+    echo "" && echo -e "A Virtual Private Network called already http-public-access exists in your project." && echo ""
+else
+    gcloud compute firewall-rules create ${2}-http-public-access \
+        --action allow \
+        --direction egress \
+        --source-ranges 0.0.0.0/0 \
+        --target-tags $2-http-public-access \
+        --network $2-vpc \
+        --rules tcp:80
+fi 
 
 ####################################### INSTANCE TEMPLATE #######################################
 
@@ -79,7 +131,7 @@ echo ""
 read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name that would you like to give your instance template:) " InstanceTemplateName
 
 if gcloud compute instance-templates describe $InstanceTemplateName &> /dev/null ; then
-    echo "" && echo -e "${RED}[ERROR 6]${WHITE} An instance template called ${InstanceTemplateName} already exists in your project." && echo "" && exit 1
+    echo "" && echo -e "${RED}[ERROR 6]${WHITE} An instance template called ${2}-vpc already exists in your project." && echo "" && exit 1
 fi
 
 #
@@ -91,12 +143,16 @@ read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the family image for
 #
 read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the project that contains the image that you want to use for your instance template:) " InstanceTemplateImageProject
 
-
 # Create Instance Template
 gcloud compute instance-templates create $InstanceTemplateName \
     --machine-type $InstanceTemplateMachineType \
     --image-family $InstanceTemplateImageFamily \
-    --image-project $InstanceTemplateImageProject
+    --image-project $InstanceTemplateImageProject \
+    --metadata-from-file=startup-script=StartupScript.sh \
+    --tags $2-http-health-check,$2-http-public-access,$2-vpc-allow \
+    --network $2-vpc \
+    --subnet $2-vpc-subnet \
+    --region $NetworkRegion
 
 echo ""
 echo -e "${GREEN}[SUCCESS]${WHITE} The instance template was created successfully."
@@ -105,63 +161,70 @@ echo ""
 ##################################### MANAGED INSTANCE GROUP ####################################
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name you want to give your Managed Instance Group:) " InstanceGroupName
+read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name you want to give your managed instance group:) " InstanceGroupName
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the region where you want your Managed Instance Group to reside in:) " InstanceGroupRegion
-
-if gcloud compute instance-groups managed describe $InstanceGroupName --zone $InstanceGroupRegion &> /dev/null; then
-    echo "" && echo -e "${RED}[ERROR 7]${WHITE} A Managed Instance Group called ${InstanceGroupName} already exists in ${InstanceTemplateRegion}." && exit 1
+if gcloud compute instance-groups managed describe $InstanceGroupName --zone $NetworkRegion &> /dev/null; then
+    echo "" && echo -e "${RED}[ERROR 7]${WHITE} A managed instance group called ${InstanceGroupName} already exists in ${InstanceTemplateRegion}." && exit 1
 fi
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name of the instance template that you want your Managed Instance Group to use:) " InstanceGroupTemplate
+read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the name of the instance template that you want your managed instance group to use:) " InstanceGroupTemplate
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the minimum number of instances that you would want your Managed Instance Group to run:) " InstanceGroupMinScaling
+read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the minimum number of instances that you would want your managed instance group to run:) " InstanceGroupMinScaling
 
 #
-read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the maximum number of instances that you would want your Managed Instance Group to scale up to:) " InstanceGroupMaxScaling
+read -p "$(echo -e ${YELLOW}[REQUIRED]${WHITE} Please enter the maximum number of instances that you would want your managed instance group to scale up to:) " InstanceGroupMaxScaling
+
+# Create Health Checks
+if gcloud compute health-checks describe http-mig-health-check; 
+then
+    echo "" && echo -e "A health check named http-health-check already exists in your project." && echo ""
+else
+    gcloud compute health-checks create http http-mig-health-check --port 80 \
+        --check-interval 30s \
+        --healthy-threshold 1 \
+        --timeout 10s \
+        --unhealthy-threshold 3 \
+        --global
+fi 
+
+echo ""
+echo -e "${GREEN}[SUCCESS]${WHITE} A health check was successfully created and configured for your managed instance group."
+echo ""
 
 # Create Instance Managed Group
 gcloud compute instance-groups managed create $InstanceGroupName \
-    --region $InstanceGroupRegion \
+    --region $NetworkRegion \
     --template $InstanceGroupTemplate \
-    --size $InstanceGroupMinScaling
+    --size $InstanceGroupMinScaling \
+    --health-check http-mig-health-check
 
 echo ""
-echo -e "${GREEN}[SUCCESS]${WHITE} Your Managed Instance Group was created successfully."
+echo -e "${GREEN}[SUCCESS]${WHITE} Your managed instance group was created successfully."
 echo ""
 
 # Configure Autoscaling
 gcloud compute instance-groups managed set-autoscaling $InstanceGroupName \
-    --region $InstanceGroupRegion \
+    --region $NetworkRegion \
     --max-num-replicas $InstanceGroupMaxScaling \
     --target-cpu-utilization 0.60 \
     --cool-down-period 90
 
 echo ""
-echo -e "${GREEN}[SUCCESS]${WHITE} Autoscaling was successfully configured for your Managed Instance Group."
-echo ""
-
-# Configure Health Checks
-#gcloud compute health-checks create http example-check --port 80 \
-#   --check-interval 30s \
-#   --healthy-threshold 1 \
-#   --timeout 10s \
-#   --unhealthy-threshold 3 \
-#   --global
-
-echo ""
-echo -e "${GREEN}[SUCCESS]${WHITE} A health check was successfully created and configured for your Managed Instance Group."
+echo -e "${GREEN}[SUCCESS]${WHITE} Autoscaling was successfully configured for your managed instance group."
 echo ""
 
 ######################################### LOAD BALANCER #########################################
 
+# Create Backend Service
+
 ########################################## REFERENCES ###########################################
 
-# Instance Template - https://cloud.google.com/compute/docs/instance-templates/create-instance-templates#gcloud
-# MIG               - https://cloud.google.com/compute/docs/instance-groups/create-zonal-mig#gcloud
-# MIG Autoscaling   - https://cloud.google.com/compute/docs/instance-groups/create-mig-with-basic-autoscaling
-# MIG Health Check  - https://cloud.google.com/compute/docs/instance-groups/autohealing-instances-in-migs
-# Compute Networks  - https://cloud.google.com/sdk/gcloud/reference/compute/networks/create
+# Instance Template                   - https://cloud.google.com/compute/docs/instance-templates/create-instance-templates#gcloud
+# Managed Instance Group              - https://cloud.google.com/compute/docs/instance-groups/create-zonal-mig#gcloud
+# Managed Instance Group Autoscaling  - https://cloud.google.com/compute/docs/instance-groups/create-mig-with-basic-autoscaling
+# Managed Instance Group Health Check - https://cloud.google.com/compute/docs/instance-groups/autohealing-instances-in-migs
+# Compute Networks                    - https://cloud.google.com/sdk/gcloud/reference/compute/networks/create
+# Application Load Balancer           - https://cloud.google.com/load-balancing/docs/application-load-balancer
