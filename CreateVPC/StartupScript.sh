402a56d42d2271db6942e09c4d9a444ab001b1fb
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-25 19:22:55 -0400

    Update StartupScript.sh

--- CreateVPC/StartupScript.sh
+++ CreateVPC/StartupScript.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+apt update
+apt -y install apache2
+
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+
+#/var/www/html/index.html
