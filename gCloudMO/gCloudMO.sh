8ca0904c2e0c004fe0f3b56aa9903972c702d08d
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-26 18:46:50 -0400

    Update gCloudMO.sh

--- gCloudMO/gCloudMO.sh
+++ gCloudMO/gCloudMO.sh
@@ -1,8 +1,29 @@
 ####################################### STATIC VARIABLES ########################################
 
+# Text Color
+WHITE="\033[0m"
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+
+# Infinte Loop
+ALWAYS_TRUE=true
+
 ######################################### REQUIREMENTS ##########################################
 
 ######################################## ARGUMENTS CHECK ########################################
+
+# Check if the user executed the script correctly
+while getopts ":f:" opt; do
+    case $opt in
+        f) file="$OPTARG"
+        ;;
+        \?) echo -e "${RED}[ERROR 1]${WHITE} Usage: ./CreateVPC.sh -p <TEXT_FILE>$" && echo "" &&  exit 1
+        ;;
+        :) echo -e "${RED}[ERROR 2]${WHITE} Usage: ./CreateVPC.sh -p <TEXT_FILE>." && echo "" && exit 1
+        ;;
+    esac
+done
 
 # Verify that the projects in the provided file all exist
 
