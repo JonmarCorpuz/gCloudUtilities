2c40c2ae6a534ffd26609fc4d732684f78ae6539
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-09-29 21:50:19 -0400

    Update gCloudWB.sh

--- gCloudWB/gCloudWB.sh
+++ gCloudWB/gCloudWB.sh
@@ -26,7 +26,7 @@ create and launch a redundant web application using a simple network architectur
 '''
 
 echo -e "${YELLOW}[DISCLAIMER]${WHITE} This script was created for testing purposes and should be used in a production environment." && echo "" 
-echo -e "${YELLOW}[REQUIRED]${WHITE} Don't forget to configure your startup script using the StartupScript.sh file in this repository." && echo ""
+echo -e "${YELLOW}[REQUIRED]${WHITE} Don't forget to configure your web server in the StartupScript.sh file found in this repository." && echo ""
 
 ######################################## ARGUMENTS CHECK ########################################
 
