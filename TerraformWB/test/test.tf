920e1b45d5b9846afed2bfd49a35228d0df7d095
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-07 12:34:12 -0400

    Update test.tf

--- TerraformWB/test/test.tf
+++ TerraformWB/test/test.tf
@@ -14,32 +14,20 @@ terraform {
 }
 
 provider "google" {
-  project     = var.project_id
-  region      = var.project_region
+  project     = "PROJECT_ID"
+  region      = "northamerica-northeast1"
   credentials = "keys.json"
 }
 
 provider "google-beta" {
-  project     = var.project_id
-  region      = var.project_region
+  project     = "PROJECT_ID"
+  region      = "northamerica-northeast1"
   credentials = "keys.json"
-}
-
-variable "project_id" {
-  description = "The project's ID."
-  type        = string
-  default     = "PROJECT_ID"
-}
-
-variable "project_region" {
-  description = "The region that the project resides in."
-  type        = string
-  default     = "northamerica-northeast1"
 }
 
 // Virtual Private Cloud 
 resource "google_compute_network" "test-network" {
-  project                 = var.project_id
+  project                 = "PROJECT_ID"
   name                    = "test-vpc"
   auto_create_subnetworks = false
 }
