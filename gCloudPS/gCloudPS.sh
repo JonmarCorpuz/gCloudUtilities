09e8b9098da166b7f1b2a230faaa9720fe471e4b
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-13 16:05:47 -0400

    Update gCloudPS.sh

--- gCloudPS/gCloudPS.sh
+++ gCloudPS/gCloudPS.sh
@@ -53,44 +53,100 @@ do
     fi
 done
 
+### Hold up, let him cook
+
+gcloud projects get-iam-policy $ProjectID | grep "role:" > Roles.txt
+
+sed 's/role: $//' Roles.txt
+
+while read Role;
+do
+
+    test=$(echo "${Role##* }")
+    echo $test
+
+    Remove="projects/$ProjectID/"
+    Role1=${test//"$Remove"/}
+
+    echo "" && echo $Role1 && echo ""
+
+    if gcloud iam roles describe $Role1 &> /dev/null;
+    then
+
+        Remove3="roles/"
+        Role3=${test//"$Remove3"/}
+
+        echo "YES"
+        echo "" && echo $Role3 && echo ""
+    
+        # Predefined Role
+        touch ${Role3}.txt
+        gcloud iam roles describe $Role1 >> ${Role3}.txt
+
+        mv ${Role3}.txt $FolderName
+
+    else
+    
+        # Custom Role
+        #Remove2="projects/$ProjectID/roles/"
+        #Role2=${test//"$Remove2"/}
+        Remove2="roles/"
+        Role2=${Role1//"$Remove2"/}
+
+        echo "NO"
+        echo "" && echo $Role2 && echo ""
+
+        touch ${Role2}.txt
+        gcloud iam roles describe $Role2 --project $ProjectID >> ${Role2}.txt
+
+        mv ${Role2}.txt $FolderName
+
+    fi
+
+done < Roles.txt
+
+gcloud asset search-all-iam-policies --scope=projects/$ProjectID | grep "user:" > Users.txt
+gcloud asset search-all-iam-policies --scope=projects/$ProjectID | grep "gserviceaccount" > ServiceAccounts.txt
+
+mv Users.txt $FolderName
+mv ServiceAccounts.txt $FolderName
+
 ######################################## USER ACCOUNTS ##########################################
 
 # List all users on the project
 
-gcloud asset search-all-iam-policies --scope=projects/$ProjectID | grep "user:" > Users.txt
-
-#echo "" && echo "pleasework" && echo ""
+#gcloud asset search-all-iam-policies --scope=projects/$ProjectID | grep "user:" > Users.txt
 
 # Get each user's role
-while read User;
-do
+#while read User;
+#do
 
     # Get user email only
-    UserEmail=$(echo "${User##* }")
+#    UserEmail=$(echo "${User##* }")
 
-    Remove3="user:"
-    TestUserEmail=${UserEmail//"$Remove3"/}
-    Remove4="@gmail.com"
-    Username=${TestUserEmail//"$Remove4"/}
+#    Remove3="user:"
+#    TestUserEmail=${UserEmail//"$Remove3"/}
+#    Remove4="@gmail.com"
+#    Username=${TestUserEmail//"$Remove4"/}
 
-    Filename="USER@${Username}"
+#    Filename="USER@${Username}"
     #echo $Filename
 
-    touch ${Filename}.txt
+#    touch ${Filename}.txt
 
-    echo "" >> ${Filename}.txt
-    echo "=== User Information ==========" >> ${Filename}.txt
-    echo "- User: ${UserEmail}" >> ${Filename}.txt
-    echo "" >> ${Filename}.txt
+#    echo "" >> ${Filename}.txt
+#    echo "=== User Information ==========" >> ${Filename}.txt
+#    echo "- User: ${UserEmail}" >> ${Filename}.txt
+#    echo "" >> ${Filename}.txt
 
     # Fetch user's role
-    UserRoleRaw=$(gcloud asset analyze-iam-policy --project=$ProjectID --identity=$UserEmail | grep "role")
-    UserRole=$(echo "${UserRoleRaw##* }")
+#    UserRoleRaw=$(gcloud asset analyze-iam-policy --project=$ProjectID --identity=$UserEmail | grep "role")
+#    UserRole=$(echo "${UserRoleRaw##* }")
 
     #echo "" && echo "1" && echo ""
 
-    Remove="projects/$ProjectID/"
-    Role=${UserRole//"$Remove"/}
+#    Remove="projects/$ProjectID/"
+#    Role=${UserRole//"$Remove"/}
 
     #echo $UserRoleRaw
     #echo $UserRole
@@ -99,171 +155,122 @@ do
     #echo "" && echo "2" && echo ""
     
     # List role's permissions
-    if gcloud iam roles describe $Role &> /dev/null;
-    then
+#    if gcloud iam roles describe $Role &> /dev/null;
+#    then
     
         # Predefined Role
-        gcloud iam roles describe $Role >> PermissionsRaw-$UserEmail.yaml
-        echo "=== Role Summary ==============" >> ${Filename}.txt
-        echo "- Role type: Predefined" >> ${Filename}.txt
-        echo "- Role:      ${Role}" >> ${Filename}.txt
+#        gcloud iam roles describe $Role >> PermissionsRaw-$UserEmail.yaml
+#        echo "=== Role Summary ==============" >> ${Filename}.txt
+#        echo "- Role type: Predefined" >> ${Filename}.txt
+#        echo "- Role:      ${Role}" >> ${Filename}.txt
 
-    else
+#    else
     
         # Custom Role
-        Remove2="projects/$ProjectID/roles/"
-        Role2=${UserRole//"$Remove2"/}
+#        Remove2="projects/$ProjectID/roles/"
+#        Role2=${UserRole//"$Remove2"/}
 
-        gcloud iam roles describe $Role2 --project $ProjectID >> PermissionsRaw-$UserEmail.yaml
+#        gcloud iam roles describe $Role2 --project $ProjectID >> PermissionsRaw-$UserEmail.yaml
 
-        echo "=== Role Summary ==============" >> ${Filename}.txt
-        echo "- Role type: Custom" >> ${Filename}.txt
-        echo "- Role:      ${Role}" >> ${Filename}.txt
+#        echo "=== Role Summary ==============" >> ${Filename}.txt
+#        echo "- Role type: Custom" >> ${Filename}.txt
+#        echo "- Role:      ${Role}" >> ${Filename}.txt
 
-    fi
+#    fi
 
     #echo "" && echo "3" && echo ""
 
-    cat PermissionsRaw-$UserEmail.yaml | grep "-" > Permissions-$UserEmail.yaml
+#    cat PermissionsRaw-$UserEmail.yaml | grep "-" > Permissions-$UserEmail.yaml
 
     # List the resources that the user can access
     #Create a list and append every resource that you see
 
-    touch AllowedResources-$UserEmail.txt
+#    touch AllowedResources-$UserEmail.txt
 
-    while read Permission;
-    do
+#    while read Permission;
+#    do
 
         # Split the permission, ex: accessapproval.requests.approve, into three parts and take the first
-        FullPermission=$(echo "${Permission##* }")
-        Resource=$(echo $FullPermission | tr "." "\n" | head -n 1)
+#        FullPermission=$(echo "${Permission##* }")
+#        Resource=$(echo $FullPermission | tr "." "\n" | head -n 1)
 
         #echo $Resource
 
         # If file doesn't include the resource, add it, else nah
-        if ! grep -Fxq "${Resource}" AllowedResources-$UserEmail.txt &> /dev/null;
-        then
-            echo "- ${Resource}" >> AllowedResources-$UserEmail.txt
-        fi
+#        if ! grep -Fxq "${Resource}" AllowedResources-$UserEmail.txt &> /dev/null;
+#        then
+#            echo "- ${Resource}" >> AllowedResources-$UserEmail.txt
+#        fi
 
-    done < Permissions-$UserEmail.yaml
+#    done < Permissions-$UserEmail.yaml
 
-    sed -i '1d;$d' AllowedResources-$UserEmail.txt
+#    sed -i '1d;$d' AllowedResources-$UserEmail.txt
 
-    echo "" >> ${Filename}.txt
-    echo "=== Accessible Resources ======" >> ${Filename}.txt
-    cat AllowedResources-$UserEmail.txt >> ${Filename}.txt
+#    echo "" >> ${Filename}.txt
+#    echo "=== Accessible Resources ======" >> ${Filename}.txt
+#    cat AllowedResources-$UserEmail.txt >> ${Filename}.txt
 
-    echo "" >> ${Filename}.txt
-    echo "=== Permissions ===============" >> ${Filename}.txt
-    cat Permissions-$UserEmail.yaml | grep "-" >> ${Filename}.txt
-    echo "" >> ${Filename}.txt
+#    echo "" >> ${Filename}.txt
+#    echo "=== Permissions ===============" >> ${Filename}.txt
+#    cat Permissions-$UserEmail.yaml | grep "-" >> ${Filename}.txt
+#    echo "" >> ${Filename}.txt
 
-    mv ${Filename}.txt $FolderName
+#    mv ${Filename}.txt $FolderName
 
-    rm PermissionsRaw-$UserEmail.yaml
-    rm Permissions-$UserEmail.yaml
-    rm AllowedResources-$UserEmail.txt
+#    rm PermissionsRaw-$UserEmail.yaml
+#    rm Permissions-$UserEmail.yaml
+#    rm AllowedResources-$UserEmail.txt
 
-done < Users.txt
+#done < Users.txt
 
-mv Users.txt $FolderName
+#mv Users.txt $FolderName
 
 ###################################### SERVICE ACCOUNTS #########################################
 
-gcloud iam service-accounts list --project $ProjectID | grep "EMAIL:" > ServiceAccountsRaw.yaml
+#gcloud iam service-accounts list --project $ProjectID | grep "EMAIL:" > ServiceAccountsRaw.yaml
 
-while read ServiceAccount;
-do
+#while read ServiceAccount;
+#do
 
-    echo "${ServiceAccount##* }" >> ServiceAccounts.txt
+#    echo "${ServiceAccount##* }" >> ServiceAccounts.txt
     
-done < ServiceAccountsRaw.yaml
+#done < ServiceAccountsRaw.yaml
 
-while read ServiceAccount;
-do
-    gcloud iam service-accounts describe $ServiceAccount --project $ProjectID
-    echo ""
-done < ServiceAccounts.txt
+#while read ServiceAccount;
+#do
+#    gcloud iam service-accounts describe $ServiceAccount --project $ProjectID
+#    echo ""
+#done < ServiceAccounts.txt
 
 ### Roles
 
-gcloud projects get-iam-policy $ProjectID | grep "role:" > Roles.txt
+#gcloud projects get-iam-policy $ProjectID | grep "role:" > Roles.txt
 
-while read Role;
-do
+#while read Role;
+#do
 
-    if gcloud iam roles describe $Role &> /dev/null;
-    then
+#    if gcloud iam roles describe $Role &> /dev/null;
+#    then
     
         # Predefined Role
-        gcloud iam roles describe $Role >> $Role.txt
-        echo "=== Role Summary ==============" >> test123.txt
-        echo "- Role type: Predefined" >> test123.txt
-        echo "- Role:      ${Role}" >> test123.txt
-
-    else
-    
-        # Custom Role
-        Remove2="projects/$ProjectID/roles/"
-        Role2=${Role//"$Remove2"/}
-
-        gcloud iam roles describe $Role2 --project $ProjectID >> $Role.txt
-
-        echo "=== Role Summary ==============" >> test123.txt
-        echo "- Role type: Custom" >> test123.txt
-        echo "- Role:      ${Role}" >> test123.txt
-
-    fi
-
-done < Roles.txt
-
-### Hold up, let him cook
-
-gcloud projects get-iam-policy jonmardemoproject | grep "role:" > Roles.txt
-
-sed 's/role: $//' Roles.txt
-
-
-while read Role;
-do
-
-#    Remove123="role:"
-#    tmp=${Role//"Remove123"/}
-
-#    echo $tmp
-
-    test=$(echo "${Role##* }")
-    echo $test
-
-    Remove="projects/jonmardemoproject/"
-    Role1=${test//"$Remove"/}
-
-#    echo $Role1
-
-    if gcloud iam roles describe $Role1 &> /dev/null;
-    then
-    
-        # Predefined Role
-        gcloud iam roles describe $Role1 >> gg123.txt
+#        gcloud iam roles describe $Role >> $Role.txt
 #        echo "=== Role Summary ==============" >> test123.txt
 #        echo "- Role type: Predefined" >> test123.txt
 #        echo "- Role:      ${Role}" >> test123.txt
 
-    else
+#    else
     
         # Custom Role
-        Remove2="projects/jonmardemoproject/roles/"
-        Role2=${test//"$Remove2"/}
+#        Remove2="projects/$ProjectID/roles/"
+#        Role2=${Role//"$Remove2"/}
 
-        echo $Role2
+#        gcloud iam roles describe $Role2 --project $ProjectID >> $Role.txt
 
-        gcloud iam roles describe $Role2 --project jonmardemoproject >> gg456.txt
+#        echo "=== Role Summary ==============" >> test123.txt
+#        echo "- Role type: Custom" >> test123.txt
+#        echo "- Role:      ${Role}" >> test123.txt
 
-#        echo "=== Role Summary ==============" >> test456.txt
-#        echo "- Role type: Custom" >> test456.txt
-#        echo "- Role:      ${Role}" >> test456.txt
+#    fi
 
-    fi
+#done < Roles.txt
 
-done < Roles.txt
