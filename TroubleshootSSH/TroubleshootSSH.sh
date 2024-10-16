6e64b4c4901b2533a24bc6ec430e3fe293639aa4
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-16 18:00:42 -0400

    Update TroubleshootSSH.sh

--- TroubleshootSSH/TroubleshootSSH.sh
+++ TroubleshootSSH/TroubleshootSSH.sh
@@ -1,10 +1,20 @@
+# Query for VM instance information
+
 # Check if user has necessary permmissions
+
+gcloud asset search-all-iam-policies --scope=projects/<PROJECT_ID> --query="policy:<USER_EMAIL>" | grep "role"
 
 # Check if service account has necessary permissions
 
+gcloud asset search-all-iam-policies --scope=projects/<PROJECT_ID> --query="policy:<SERVICE_ACCOUNT_EMAIL>" | grep "role"
+
 # Test in-browser SSH ?
 
+gcloud compute ssh <INSTANCE_NAME> --zone <INSTANCE_ZONE> --command exit
+
 # Test SSH through IAP ?
+
+gcloud compute ssh <INSTANCE_NAME> --tunnel-through-iap --zone <INSTANCE_ZONE> --command exit
 
 # Test domain SSH ?
 
