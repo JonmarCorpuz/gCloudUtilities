025215b997df5aa1e1e8ae5fcbdc243a37c7daae
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-16 18:13:03 -0400

    Update gCloudAD.sh

--- gCloudAD/gCloudAD.sh
+++ gCloudAD/gCloudAD.sh
@@ -5,4 +5,4 @@ gcloud active-directory domains create <FQDN> \
     --reserved-ip-range=<CIDR_RANGE> --region=<REGION> \
     --authorized-networks=projects/<PROJECT_ID>/global/networks/<VPC_NETWORK_NAME>
 
-# Join Linux VM to domain
+# Join Linux VM to domain (Execute commands through SSH)
