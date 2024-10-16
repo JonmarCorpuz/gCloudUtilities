82a77eb59913e484aaa2a314593ce886ab946abe
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-16 18:12:34 -0400

    Create gCloudAD.sh

---
+++ gCloudAD/gCloudAD.sh
@@ -0,0 +1,8 @@
+
+# Create AD domain
+
+gcloud active-directory domains create <FQDN> \
+    --reserved-ip-range=<CIDR_RANGE> --region=<REGION> \
+    --authorized-networks=projects/<PROJECT_ID>/global/networks/<VPC_NETWORK_NAME>
+
+# Join Linux VM to domain
