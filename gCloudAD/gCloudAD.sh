1ba2ac7a62e280a91addd2ee8c6aac8c246bb088
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-17 19:17:38 -0400

    Update gCloudAD.sh

--- gCloudAD/gCloudAD.sh
+++ gCloudAD/gCloudAD.sh
@@ -50,7 +50,6 @@ then
 fi
 
 # Create AD domain
-
 gcloud active-directory domains create <FQDN> \
     --reserved-ip-range=<CIDR_RANGE> --region=<REGION> \
     --authorized-networks=projects/<PROJECT_ID>/global/networks/<VPC_NETWORK_NAME>
