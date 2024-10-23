9cf3e7b806d93f1310402c1e5a415c9688f8b4e4
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-10-23 17:57:34 -0400

    Update gCloudKP.sh

--- gCloudKP/gCloudKP.sh
+++ gCloudKP/gCloudKP.sh
@@ -27,3 +27,14 @@ if ! gcloud services list | grep "container.googleapis.com";
 then
     gcloud services enable container.googleapis.com
 fi
+
+# Ensure that kubectl is installed
+if ! kubectl version --client;
+then 
+    gcloud components install kubectl
+fi
+
+# Ensure that the required plugin is installed
+if ! gke-gcloud-auth-plugin --version;
+then
+    gcloud components install gke-cloud-auth-plugin
