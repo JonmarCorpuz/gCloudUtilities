b02f49b990fbf728eb376300982ce3cf5dd6cd40
Jonmar Corpuz <121370426+JonmarCorpuz@users.noreply.github.com>
2024-12-21 14:47:46 -0500

    Delete tFormWB directory

--- tFormWB/StartupScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
-sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-sudo systemctl start google-cloud-ops-agent"*"
-
-sudo apt -y install apache2
-sudo apt -y update
--- tFormWB/tests/test.tftest.hcl
+++
@@ -1,5 +0,0 @@
-run "test" {
-    module {
-        source = "./setup"
-    }
-}
